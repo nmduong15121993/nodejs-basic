9d4227356d3104e47fea65b8401e4d039e195ef6
Duong Nguyen <nmduong15121993@gmail.com>
2021-10-14 00:30:05 +0700

    validateion

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 let userRoutes = require('./routes/user.route');
-
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'pug');
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
   res.render('index', { name: 'Duong' });
