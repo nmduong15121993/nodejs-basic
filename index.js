6a9b1a42c81b8e0ea7be678bd1e6ff37a7fdc4a8
Duong Nguyen <nmduong15121993@gmail.com>
2021-10-13 08:46:52 +0700

    add lowdb package

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const app = express();
+
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+// Set default data
+db.defaults({ dataUsers: [] })
+  .write();
+
 const port = 3000;
 
 app.set('view engine', 'pug');
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-let dataUsers = [
-  {id: 1, name: 'Duong'},
-  {id: 2, name: 'Nguyen'},
-  {id: 3, name: 'Minh'},
-];
 
 app.get('/', function(req, res) {
   res.render('index', { name: 'Duong' });
@@ -17,18 +24,28 @@ app.get('/', function(req, res) {
 app.get('/users', function(req, res) {
   res.render('users/index', {
     title: 'Danh Sach Users',
-    users: dataUsers,
+    users: db.get('dataUsers').value(),
   });
 });
 
 app.get('/users/search', function(req, res) {
   const q = req.query.q;
+  const dataUsers = db.get('dataUsers').value();
   let matchedUsers = dataUsers.filter(function(user) {
     return user.name.toUpperCase().indexOf(q.toUpperCase()) !== -1;
   })
   res.render('users/index', {
     users: matchedUsers,
   })
+})
+
+app.get('/users/create', function(req, res) {
+  res.render('users/create');
+})
+
+app.post('/users/create', function(req, res) {
+  db.get('dataUsers').push(req.body).write();
+  res.redirect('/users');
 })
 
 app.listen(port, () => console.log('App listen on port 3000'));
