7de2f67df367d830db54beeb68b94a884729f363
Duong Nguyen <nmduong15121993@gmail.com>
2021-10-14 08:41:32 +0700

    midleware

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,6 +2,7 @@ let express = require('express');
 
 const db = require('../db');
 let constroller = require('../controllers/user.controller');
+let validate = require('../validate/user.validate');
 
 const router = express.Router();
 
@@ -9,6 +10,6 @@ router.get('/', constroller.index);
 router.get('/search', constroller.search);
 router.get('/create', constroller.create);
 router.get('/:id', constroller.get);
-router.post('/create', constroller.postCreate);
+router.post('/create', validate.postCreate, constroller.postCreate);
 
 module.exports = router;
